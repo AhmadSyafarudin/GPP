762485ff202c31d41e1dd2acc86062d2bd26b3e7
AhmadSyafarudin <ahmadsyafar123@gmail.com>
2022-10-10 00:35:15 +0700

    Fix CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-//Screen Components
 import Home from "./Pages/Home/Home";
 import Admin from "./Pages/Admin/Admin";
 import Login from "./Pages/Admin/components/auth/Login";
 import Sign from "./Pages/Admin/components/auth/Sign";
 import { AuthProvider } from "./context/AuthContext";
-import KosanList from './Pages/Admin/components/KosanList';
 import KosanAdd from './Pages/Admin/components/KosanAdd';
 import KosanEdit from './Pages/Admin/components/KosanEdit';
 import KosanView from './Pages/View/KosanView';
